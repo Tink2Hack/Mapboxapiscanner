abd454e03b77c11a5587afbe2a9a46b4d80df7c6
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-09-14 14:12:06 +0200

    Use argparse and make domain configurable

--- MAPBOX_API_SCANNER.py
+++ MAPBOX_API_SCANNER.py
@@ -2,45 +2,46 @@ import requests
 import sys
 import warnings
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("api_token")
+parser.add_argument("-d", "--domain", default="api.mapbox.com")
+args = parser.parse_args()
+
 warnings.filterwarnings("ignore")
 
-if len(sys.argv) <= 1:
-        print("Usage: python MAPBOX_API_SCANNER.py <api-token>")
-        quit()
-
-apikey = sys.argv[1]
+apikey = args.api_token
 
 def endpoint_vulnerable(url):
         response = requests.get(url, verify=False)
         return response.status_code == 200
 
+def check_endpoint(url):
+    if endpoint_vulnerable(url): print("Vulnerable",[url])
+    else: print("Not Vulnerable")
+
+
 print("Vector Tiles API\t", end="")
-url = "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/1/0/0.mvt?access_token="+apikey
-if endpoint_vulnerable(url): print("Vulnerable",[url])
-else: print("Not Vulnerable")
+url = "https://{}/v4/mapbox.mapbox-streets-v8/1/0/0.mvt?access_token={}".format(args.domain, apikey)
+check_endpoint(url)
 
 print("Raster Tiles API\t", end="")
-url = "https://api.mapbox.com/v4/mapbox.satellite/1/0/0@2x.jpg90?access_token="+apikey
-if endpoint_vulnerable(url): print("Vulnerable",[url])
-else: print("Not Vulnerable")
+url = "https://{}/v4/mapbox.satellite/1/0/0@2x.jpg90?access_token={}".format(args.domain, apikey)
+check_endpoint(url)
 
 print("Static Images API\t", end="")
-url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/url-https%3A%2F%2Fwww.mapbox.com%2Fimg%2Frocket.png(-76.9,38.9)/-76.9,38.9,15/1000x1000?access_token="+apikey
-if endpoint_vulnerable(url): print("Vulnerable",[url])
-else: print("Not Vulnerable")
+url = "https://{}/styles/v1/mapbox/streets-v11/static/url-https%3A%2F%2Fwww.mapbox.com%2Fimg%2Frocket.png(-76.9,38.9)/-76.9,38.9,15/1000x1000?access_token={}".format(args.domain, apikey)
+check_endpoint(url)
 
 print("Static Tiles API\t", end="")
-url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/512/1/1/0@2x?access_token="+apikey
-if endpoint_vulnerable(url): print("Vulnerable",[url])
-else: print("Not Vulnerable")
+url = "https://{}/styles/v1/mapbox/streets-v11/tiles/512/1/1/0@2x?access_token={}".format(args.domain, apikey)
+check_endpoint(url)
 
 print("Styles API\t\t", end="")
-url = "https://api.mapbox.com/styles/v1/examples/cjikt35x83t1z2rnxpdmjs7y7?access_token="+apikey
-if endpoint_vulnerable(url): print("Vulnerable",[url])
-else: print("Not Vulnerable")
+url = "https://{}/styles/v1/examples/cjikt35x83t1z2rnxpdmjs7y7?access_token={}".format(args.domain, apikey)
+check_endpoint(url)
 
 print("Tilequery API\t\t", end="")
-url = "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/-122.42901,37.80633.json?radius=10&access_token="+apikey
-if endpoint_vulnerable(url): print("Vulnerable",[url])
-else: print("Not Vulnerable")
+url = "https://{}/v4/mapbox.mapbox-streets-v8/tilequery/-122.42901,37.80633.json?radius=10&access_token={}".format(args.domain, apikey)
+check_endpoint(url)
 
